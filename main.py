3f57667a5a9b599966bdf6b6ebab7ffed4e26ce4
eriksoaress <erikbtu2020@gmail.com>
2023-03-30 19:18:49 -0300

    arrumando cubo eixo z

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def inicializa():
 
 
 def matrizP(d):
-    return np.array([[1,0,0],[0,1,0],[0,0,-1/d]])
+    return np.array([[1,0,0,0],[0,1,0,0],[0,0,0,-d],[0,0,-1/d,0]])
 
 
 
@@ -26,16 +26,22 @@ def finaliza():
 
 def desenha(window: pygame.Surface):
 
-    pontos_cubo =np.array([[100,100,1],[100,-100, 1],[-100,-100,1],[-100,100,1], [100,100,1],[100,-100,1],[-100,-100,1],[-100,100,1]])
-    translacao = np.array([[1,0,360],[0,1,360],[0,0,1]])
+    pontos_cubo =np.array([[100,100,100,1],[100,-100, 100,1],[-100,-100,100,1],[-100,100,100,1], [100,100,300,1],[100,-100,300,1],[-100,-100,300,1],[-100,100,300,1]])
+    translacao = np.array([[1,0,360,0],[0,1,360,0],[0,0,1,0]])
     pontos_projecao = matrizP(1)@pontos_cubo.T
     pontos_projecao = translacao@pontos_projecao
     print(pontos_projecao)
     
-    print( pontos_projecao[0][0]/pontos_projecao[2][0] , pontos_projecao[1][0]/pontos_projecao[2][0]), (pontos_projecao[0][1]/pontos_projecao[2][1], pontos_projecao[1][1]/pontos_projecao[2][1])
 
+    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][0]/pontos_projecao[2][0] , pontos_projecao[1][0]/pontos_projecao[2][0]), ( pontos_projecao[0][1]/pontos_projecao[2][1],  pontos_projecao[1][1]/pontos_projecao[2][1]), 3)
+    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][1]/pontos_projecao[2][1] , pontos_projecao[1][1]/pontos_projecao[2][1]), ( pontos_projecao[0][2]/pontos_projecao[2][2] , pontos_projecao[1][2]/pontos_projecao[2][2]), 3)
+    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][2]/pontos_projecao[2][2] , pontos_projecao[1][2]/pontos_projecao[2][2]), ( pontos_projecao[0][3]/pontos_projecao[2][3] , pontos_projecao[1][3]/pontos_projecao[2][3]), 3)
+    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][3]/pontos_projecao[2][3] , pontos_projecao[1][3]/pontos_projecao[2][3]), ( pontos_projecao[0][0]/pontos_projecao[2][0] , pontos_projecao[1][0]/pontos_projecao[2][0]), 3)
 
-    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][0]/pontos_projecao[2][0] , pontos_projecao[1][0]/pontos_projecao[2][0]), (pontos_projecao[0][1]/pontos_projecao[2][1], pontos_projecao[1][1]/pontos_projecao[2][1]), 3)
+    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][4]/pontos_projecao[2][4] , pontos_projecao[1][4]/pontos_projecao[2][4]),  (pontos_projecao[0][5]/pontos_projecao[2][5],  pontos_projecao[1][5]/pontos_projecao[2][5]), 3)
+    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][5]/pontos_projecao[2][5] , pontos_projecao[1][5]/pontos_projecao[2][5]), ( pontos_projecao[0][6]/pontos_projecao[2][6] , pontos_projecao[1][6]/pontos_projecao[2][6]), 3)
+    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][6]/pontos_projecao[2][6] , pontos_projecao[1][6]/pontos_projecao[2][6]), ( pontos_projecao[0][7]/pontos_projecao[2][7] , pontos_projecao[1][7]/pontos_projecao[2][7]), 3)
+    pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][7]/pontos_projecao[2][7] , pontos_projecao[1][7]/pontos_projecao[2][7]), ( pontos_projecao[0][4]/pontos_projecao[2][4] , pontos_projecao[1][4]/pontos_projecao[2][4]), 3)
 
     pygame.display.update()
 
