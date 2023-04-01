cc502961dba2e2c4ec75dbdc9149609e659541a0
fernandovs4 <fervsantos3000@gmail.com>
2023-04-01 01:04:06 -0300

    cubo rotacionando

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def inicializa():
     # Inicializa o Pygame
     pygame.init()
     window = pygame.display.set_mode((720, 720), flags=pygame.SCALED)
-    states={"angulo":0}
+    states={"angulo":0, 'd': 200}
 
    
 
@@ -19,10 +19,17 @@ def inicializa():
 def matrizP(d):
     return np.array([[1,0,0,0],[0,1,0,0],[0,0,0,-d],[0,0,-1/d,0]])
 
-def rotacao(angulo):
+def rotacao_Z(angulo):
     return np.array([[np.cos(angulo),-np.sin(angulo),0,0],[np.sin(angulo),np.cos(angulo),0,0],[0,0,1,0],[0,0,0,1]])
 
+def rotacao_Y(angulo):
+    return np.array([[np.cos(angulo),0,np.sin(angulo),0],[0,1,0,0],[-np.sin(angulo),0,np.cos(angulo),0],[0,0,0,1]])
 
+def rotacao_X(angulo):
+    return np.array([[1,0,0,0],[0,np.cos(angulo),-np.sin(angulo),0],[0,np.sin(angulo),np.cos(angulo),0],[0,0,0,1]])
+
+def translacao_pra_origem(h_cubo, lado_cubo):
+    return np.array([[1,0,0,0],[0,1,0,0],[0,0,1,-h_cubo - lado_cubo/2],[0,0,0,1]])
 
 def finaliza():
     '''Função utilizada para fechar o pygame'''
@@ -33,13 +40,17 @@ def desenha(window: pygame.Surface, states):
 
   
     window.fill((0, 0, 0))
-    pontos_cubo =np.array([[100,100,100,1],[100,-100, 100,1],[-100,-100,100,1],[-100,100,100,1], [100,100,300,1],[100,-100,300,1],[-100,-100,300,1],[-100,100,300,1]])
-    translacao = np.array([[1,0,0,200],[0,1,0,200],[0,0,1,0],[0,0,0,1]])
-    pontos_projecao = matrizP(100)@pontos_cubo.T
-    pontos_projecao = rotacao(states['angulo'])@pontos_projecao
-    pontos_projecao = translacao@pontos_projecao
+    
+    pontos_cubo = np.array([[100,100,200,1],[100,-100, 200,1],[-100,-100,200,1],[-100,100,200,1], [100,100,400,1],[100,-100,400,1],[-100,-100,400,1],[-100,100,400,1]])
+    pontos_cubo =  translacao_pra_origem(200,200)@ pontos_cubo.T
+    pontos_cubo =  rotacao_Y(states["angulo"]) @ pontos_cubo
+    pontos_cubo =  np.linalg.inv(translacao_pra_origem(200,200))@ pontos_cubo
 
-    print(pontos_projecao)
+    translacao = np.array([[1,0,0,300],[0,1,0,300],[0,0,1,0],[0,0,0,1]])
+    pontos_projecao = matrizP(states['d'])@ pontos_cubo 
+    pontos_projecao = translacao@ pontos_projecao
+
+   
     for i in range(0,3):
         pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][i]/pontos_projecao[3][i] , pontos_projecao[1][i]/pontos_projecao[3][i]), ( pontos_projecao[0][i + 1]/pontos_projecao[3][i + 1] , pontos_projecao[1][i + 1]/pontos_projecao[3][i + 1]), 3)
         pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][i + 4]/pontos_projecao[3][i + 4] , pontos_projecao[1][i + 4]/pontos_projecao[3][i + 4]), ( pontos_projecao[0][i + 5]/pontos_projecao[3][i + 5] , pontos_projecao[1][i + 5]/pontos_projecao[3][i + 5]), 3)
@@ -49,19 +60,6 @@ def desenha(window: pygame.Surface, states):
     for i in range(4):
         pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][i]/pontos_projecao[3][i] , pontos_projecao[1][i]/pontos_projecao[3][i]), ( pontos_projecao[0][i + 4]/pontos_projecao[3][i + 4] , pontos_projecao[1][i + 4]/pontos_projecao[3][i + 4]), 3)
     
- 
-    
-
-    # pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][0]/pontos_projecao[2][0] , pontos_projecao[1][0]/pontos_projecao[2][0]), ( pontos_projecao[0][1]/pontos_projecao[2][1],  pontos_projecao[1][1]/pontos_projecao[2][1]), 3)
-    # pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][1]/pontos_projecao[2][1] , pontos_projecao[1][1]/pontos_projecao[2][1]), ( pontos_projecao[0][2]/pontos_projecao[2][2] , pontos_projecao[1][2]/pontos_projecao[2][2]), 3)
-    # pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][2]/pontos_projecao[2][2] , pontos_projecao[1][2]/pontos_projecao[2][2]), ( pontos_projecao[0][3]/pontos_projecao[2][3] , pontos_projecao[1][3]/pontos_projecao[2][3]), 3)
-    # pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][3]/pontos_projecao[2][3] , pontos_projecao[1][3]/pontos_projecao[2][3]), ( pontos_projecao[0][0]/pontos_projecao[2][0] , pontos_projecao[1][0]/pontos_projecao[2][0]), 3)
-
-    # pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][4]/pontos_projecao[2][4] , pontos_projecao[1][4]/pontos_projecao[2][4]),  (pontos_projecao[0][5]/pontos_projecao[2][5],  pontos_projecao[1][5]/pontos_projecao[2][5]), 3)
-    # pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][5]/pontos_projecao[2][5] , pontos_projecao[1][5]/pontos_projecao[2][5]), ( pontos_projecao[0][6]/pontos_projecao[2][6] , pontos_projecao[1][6]/pontos_projecao[2][6]), 3)
-    # pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][6]/pontos_projecao[2][6] , pontos_projecao[1][6]/pontos_projecao[2][6]), ( pontos_projecao[0][7]/pontos_projecao[2][7] , pontos_projecao[1][7]/pontos_projecao[2][7]), 3)
-    # pygame.draw.line(window, (255, 0, 0), ( pontos_projecao[0][7]/pontos_projecao[2][7] , pontos_projecao[1][7]/pontos_projecao[2][7]), ( pontos_projecao[0][4]/pontos_projecao[2][4] , pontos_projecao[1][4]/pontos_projecao[2][4]), 3)
-
     pygame.display.update()
 
 def atualiza_estado(states):  
@@ -78,6 +76,10 @@ def atualiza_estado(states):
         states['angulo'] += 0.001
     if keys[pygame.K_RIGHT]:
         states['angulo'] -= 0.001
+    if keys[pygame.K_UP]:
+        states['d'] += 0.5
+    if keys[pygame.K_DOWN]:
+        states['d'] -= 0.5
 
            
        
